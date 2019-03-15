218279f236abee4b0da0e3a2ffb923f6f00c7aab
chris green <greench@gmail.com>
2019-03-15 13:04:44 -0700

    adding files

---
+++ build/precache-manifest.39395054832958843cbfb8da643f8bc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6223e813ca68b226cf27",
+    "url": "/polygon-party/static/js/runtime~main.f143837a.js"
+  },
+  {
+    "revision": "dc0504cd13969c0b018d",
+    "url": "/polygon-party/static/js/main.a08d5897.chunk.js"
+  },
+  {
+    "revision": "29f58ed99e5919cfe260",
+    "url": "/polygon-party/static/js/2.f1bea3d9.chunk.js"
+  },
+  {
+    "revision": "29f58ed99e5919cfe260",
+    "url": "/polygon-party/static/css/2.61017619.chunk.css"
+  },
+  {
+    "revision": "39e62b8378fc0c80f0df4f7818143bc8",
+    "url": "/polygon-party/index.html"
+  }
+];
